53e90cb1382bd560bbf7682f80ad669b97202c27
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2021-07-19 13:36:16 -0300

    backToTop

--- main.js
+++ main.js
@@ -57,3 +57,13 @@ scrollReveal.reveal(
     #contact .text, #contact .links`,
     { interval: 100 } 
 )
+
+/* Back to top  */
+const backToTopButton = document.querySelector('.back-to-top')
+window.addEventListener('scroll', function() {
+    if (window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+})
